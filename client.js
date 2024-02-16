45e72dd58405f480e8a68133b508076a27c5252b
ShawnArdeshiri <Shayan.Ardeshiri@edu.sait.ca>
2024-02-15 22:50:46 -0800

    ogjadoga

---
+++ client.js
@@ -0,0 +1,44 @@
+//*before number*//
+let age = "23";
+console.log(document.querySelector('number').age);
+
+//*before string*//
+let firstName = "shawn";
+
+//*before arrays*//
+
+const friends = ['mike', 'sara'];
+
+//*before object*//
+
+const shawn = {
+  firstName : 'shawn',
+  age : '23',
+  friends: ['mike', 'sara']
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//* we use the const keyword to declare variables that are not supposed to change*//
